3cbfa82993ffcc61f2fa37cdc96a154442022acc
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-09-23 10:02:00 -0700

    fix playwright config location

--- playwright.config.js
+++ playwright.config.js
@@ -1,9 +1,33 @@
-/** @type {import('@playwright/test').PlaywrightTestConfig} */
-const config = {
+import { devices } from '@playwright/test';
+
+const known_devices = {
+	chromium: devices['Desktop Chrome'],
+	firefox: devices['Desktop Firefox'],
+	webkit: devices['Desktop Safari']
+};
+const test_browser = /** @type {keyof typeof known_devices} */ (
+	process.env.KIT_E2E_BROWSER ?? 'chromium'
+);
+
+const test_browser_device = known_devices[test_browser];
+
+if (!test_browser_device) {
+	throw new Error(
+		`invalid test browser specified: KIT_E2E_BROWSER=${
+			process.env.KIT_E2E_BROWSER
+		}. Allowed values: ${Object.keys(known_devices).join(', ')}`
+	);
+}
+
+export default {
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173
-	}
-};
-
-export default config;
+	},
+	use: {
+		...test_browser_device,
+		screenshot: 'only-on-failure',
+		trace: 'retain-on-failure'
+	},
+	workers: process.env.CI ? 2 : undefined
+}
